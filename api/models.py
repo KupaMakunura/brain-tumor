8c5f13e554ad218997c1f154950f7e9298397d21
Kupakwashe Makunura <kupsamos@gmail.com>
2024-10-30 17:49:18 +0200

    added the url configs and add made the migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    sex = models.CharField(max_length=100)
+    health_history = models.CharField(max_length=100, null=True)
+    prior_treatments = models.CharField(max_length=100, null=True)
+    allergies = models.CharField(max_length=100, null=True)
+    existing_conditions = models.CharField(max_length=100, null=True)
+    family_history = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to="images/", null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
